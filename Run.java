56a55991ee9ed7c7a3c176913c7126a6db4c695d
TVHSJohnQ <johnquintana208@gmail.com>
2025-09-25 10:05:40 -0700

    v6

--- Run.java
+++ Run.java
@@ -3,16 +3,35 @@ import java.util.Scanner;
 
 public class Run {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String action;
+
         Deck deck = new Deck();
-        Player player = new Player("Griffin",100);
         ArrayList<Player> player_array = new ArrayList<>();
-        
-        
 
-        
+        while(true)
+        {
 
+            System.out.println("Players = " + player_array.size() + "/4");
+            System.out.println("Create a new player? y/n");
+            action = input.nextLine();
 
-        player.playTurn(deck);
-        
+            if(action.equals("y"))
+            {
+                System.out.print("What will you decree them? ");
+                action = input.nextLine();
+
+                if (action.equals("*"))
+                {
+                    System.out.println("Returning..");
+                }
+                else
+                {
+                    player_array.add(new Player(action, 100));
+                }
+            }
+        }
+
     }
 }

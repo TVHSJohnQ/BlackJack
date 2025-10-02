41e0e3d6ea2e059e704e32c9d94fe150ee96504e
TVHSJohnQ <johnquintana208@gmail.com>
2025-10-02 10:05:05 -0700

    v7

---
+++ BigSpace.java
@@ -0,0 +1,11 @@
+public class BigSpace
+{
+    public static void create_space()
+    {
+        int space_ammount = 50;
+        for(int s = 0; s <= 50; s++)
+        {
+            System.out.println(" ");
+        }
+    }
+}
--- Run.java
+++ Run.java
@@ -10,16 +10,19 @@ public class Run {
         Deck deck = new Deck();
         ArrayList<Player> player_array = new ArrayList<>();
 
+
+        BigSpace.create_space();
         while(true)
         {
 
             System.out.println("Players = " + player_array.size() + "/4");
-            System.out.println("Create a new player? y/n");
+            System.out.println("(Create a new player? y/n)");
             action = input.nextLine();
 
             if(action.equals("y"))
             {
-                System.out.print("What will you decree them? ");
+                BigSpace.create_space();
+                System.out.print("What will you decree them?  (Enter * to return) ");
                 action = input.nextLine();
 
                 if (action.equals("*"))
@@ -30,7 +33,15 @@ public class Run {
                 {
                     player_array.add(new Player(action, 100));
                 }
+                BigSpace.create_space();
             }
+            else if((action.equals("start") && player_array.size() > 0) || player_array.size() == 4)
+            {
+                BigSpace.create_space();
+                System.out.println("Starting Game..");
+                return;
+            }
+            
         }
 
     }
